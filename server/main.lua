cfb699a60c82497773d9917a2dcbef9c849943b5
Nezow <56295102+Nezow@users.noreply.github.com>
2020-09-16 15:00:59 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,14 +1,10 @@
 ESX = nil
-
-print("By nezow")
-
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('esx_joblisting:setJobslaughterer')
 AddEventHandler('esx_joblisting:setJobslaughterer', function(job)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
-    
     xPlayer.setJob("slaughterer", 0)  
 end)
 
@@ -16,6 +12,5 @@ RegisterServerEvent('esx_joblisting:setJobsunemployed')
 AddEventHandler('esx_joblisting:setJobsunemployed', function(job)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
-    
     xPlayer.setJob("unemployed", 0)  
 end)
